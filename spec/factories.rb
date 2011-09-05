5ff766eaa76cc30658bb8b08cffa723de63120fa
Murthy Gudipati <mgudipati@saventech.com>
2011-09-05 21:57:25 +0530

    Added user microposts

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,16 @@
+# By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
-  user.name                  "Murthy Gudipati"
-  user.email                 "mgudipati@saventech.com"
-  user.password              "l3tm3in"
-  user.password_confirmation "l3tm3in"
+  user.name                  "Michael Hartl"
+  user.email                 "mhartl@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
 end
 
 Factory.sequence :email do |n|
   "person-#{n}@example.com"
+end
+
+Factory.define :micropost do |micropost|
+  micropost.content "Foo bar"
+  micropost.association :user
 end
